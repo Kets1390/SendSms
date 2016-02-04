a224ae2605b893580b68afde29799ff08157e953
Ketan Darji <kets1390ketu@gmail.com>
2016-02-04 17:44:42 +0530

    Testing

---
+++ app/src/main/java/com/netchnosoft/sendsms/MapsActivity.java
@@ -0,0 +1,77 @@
+package com.netchnosoft.sendsms;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity  {
+
+    String sent = "SMS_SENT";
+    PendingIntent sentPI;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+
+                if (getResultCode() == RESULT_OK) {
+                    AlertDialog.Builder builder=new AlertDialog.Builder(MapsActivity.this);
+                    builder.setMessage("Sent Successfully.");
+                    builder.setPositiveButton("OK",null);
+                    builder.show();
+
+                }
+
+            }
+        }, new IntentFilter(sent));
+
+
+
+
+
+        sentPI = PendingIntent.getBroadcast(MapsActivity.this, 0,
+                new Intent(sent), 0);
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("8511076074", null,"temp", sentPI, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+
+}
